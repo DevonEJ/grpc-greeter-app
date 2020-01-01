30c6d0f738ad54debc7c9e8d5b99479c26da9dbc
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-01-01 11:41:49 +0000

    added boilerplate code for client with no requests made

--- greeter/greeter_client/client.go
+++ greeter/greeter_client/client.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"../greetpb"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+
+	// Use withInsecure option to ignore SSL certs - creates insecure connection
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("failed to connect to server: ", err)
+	}
+	// Close connection once main is finished execution
+	defer conn.Close()
+
+	c := greetpb.NewGreetServiceClient(conn)
+
+	// TODO - Implement client requests
+	fmt.Printf("created client: ", c)
 
 }
