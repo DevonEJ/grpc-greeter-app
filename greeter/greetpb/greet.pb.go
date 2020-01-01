9c524d1ed5b9ca7d0ee833d81edde65130f3f12a
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-01-01 12:24:53 +0000

    updating .pb.go after bug removal

--- greeter/greetpb/greet.pb.go
+++ greeter/greetpb/greet.pb.go
@@ -8,6 +8,8 @@ import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -165,7 +167,7 @@ func init() {
 func init() { proto.RegisterFile("greetpb/greet.proto", fileDescriptor_cd67c47c0cf51822) }
 
 var fileDescriptor_cd67c47c0cf51822 = []byte{
-	// 194 bytes of a gzipped FileDescriptorProto
+	// 215 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4e, 0x2f, 0x4a, 0x4d,
 	0x2d, 0x29, 0x48, 0xd2, 0x07, 0xd3, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xec, 0x60, 0x4e,
 	0x6a, 0x91, 0x52, 0x1e, 0x17, 0x87, 0x3b, 0x88, 0x99, 0x99, 0x97, 0x2e, 0x24, 0xc3, 0xc5, 0x99,
@@ -175,10 +177,11 @@ var fileDescriptor_cd67c47c0cf51822 = []byte{
 	0x54, 0x09, 0x66, 0xb0, 0x12, 0x74, 0x61, 0x25, 0x5b, 0x2e, 0x1e, 0xb0, 0x7d, 0x41, 0xa9, 0x85,
 	0xa5, 0xa9, 0xc5, 0x25, 0x42, 0xba, 0x5c, 0x1c, 0xe9, 0x50, 0xfb, 0xc1, 0x56, 0x72, 0x1b, 0x09,
 	0xea, 0x41, 0xdd, 0xa6, 0x07, 0x73, 0x58, 0x10, 0x5c, 0x89, 0x92, 0x36, 0x17, 0x2f, 0x54, 0x7b,
-	0x71, 0x41, 0x7e, 0x5e, 0x31, 0xd8, 0x55, 0x45, 0x50, 0x36, 0xd4, 0xc9, 0x70, 0xbe, 0x11, 0x1f,
-	0xd4, 0xae, 0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0x27, 0xce, 0x28, 0x76, 0x68, 0x58, 0x24,
-	0xb1, 0x81, 0x83, 0xc1, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0xb4, 0x9e, 0x7e, 0x30, 0x1d, 0x01,
-	0x00, 0x00,
+	0x71, 0x41, 0x7e, 0x5e, 0x31, 0xd8, 0x55, 0x45, 0x50, 0x36, 0xd4, 0xc9, 0x70, 0xbe, 0x91, 0x07,
+	0xd4, 0xae, 0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0x21, 0x0b, 0x2e, 0x56, 0x30, 0x5f, 0x48,
+	0x14, 0xd5, 0x0a, 0xa8, 0x5b, 0xa4, 0xc4, 0xd0, 0x85, 0x21, 0xe6, 0x28, 0x31, 0x38, 0x71, 0x46,
+	0xb1, 0x43, 0x43, 0x31, 0x89, 0x0d, 0x1c, 0x80, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x26,
+	0x8b, 0xd6, 0x2a, 0x57, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -193,6 +196,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type GreetServiceClient interface {
+	// Unary greeter api - define the rpc call
+	Greet(ctx context.Context, in *GreetRequest, opts ...grpc.CallOption) (*GreetResponse, error)
 }
 
 type greetServiceClient struct {
@@ -203,22 +208,60 @@ func NewGreetServiceClient(cc *grpc.ClientConn) GreetServiceClient {
 	return &greetServiceClient{cc}
 }
 
+func (c *greetServiceClient) Greet(ctx context.Context, in *GreetRequest, opts ...grpc.CallOption) (*GreetResponse, error) {
+	out := new(GreetResponse)
+	err := c.cc.Invoke(ctx, "/greeter.GreetService/Greet", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GreetServiceServer is the server API for GreetService service.
 type GreetServiceServer interface {
+	// Unary greeter api - define the rpc call
+	Greet(context.Context, *GreetRequest) (*GreetResponse, error)
 }
 
 // UnimplementedGreetServiceServer can be embedded to have forward compatible implementations.
 type UnimplementedGreetServiceServer struct {
 }
 
+func (*UnimplementedGreetServiceServer) Greet(ctx context.Context, req *GreetRequest) (*GreetResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Greet not implemented")
+}
+
 func RegisterGreetServiceServer(s *grpc.Server, srv GreetServiceServer) {
 	s.RegisterService(&_GreetService_serviceDesc, srv)
+}
+
+func _GreetService_Greet_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GreetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreetServiceServer).Greet(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/greeter.GreetService/Greet",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreetServiceServer).Greet(ctx, req.(*GreetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _GreetService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "greeter.GreetService",
 	HandlerType: (*GreetServiceServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "greetpb/greet.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Greet",
+			Handler:    _GreetService_Greet_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "greetpb/greet.proto",
 }
